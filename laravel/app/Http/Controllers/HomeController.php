45ab79d66ac0b90278b25dad023d1b7340cf0f1d
Yuriy Zhizhnevskiy <yury.zhyzhneuski@innowise-group.com>
2021-10-09 11:54:41 +0300

    update learn

--- laravel/app/Http/Controllers/HomeController.php
+++ laravel/app/Http/Controllers/HomeController.php
@@ -40,30 +40,34 @@ class HomeController extends Controller
 //        );
 //        var_dump($delete);
 
-        DB::beginTransaction();
-        try {
-           DB::update('
-        UPDATE posts SET created_at = :create WHERE created_at IS NULL
-        ', ['create' => NOW()]);
-           DB::update('
-        UPDATE posts SET updated_at = :update WHERE updated_at IS NULL
-        ', ['update' => NOW()]);
-           DB::commit();
-        } catch (\Exception $e){
-            DB::rollBack();
-            echo $e->getMessage();
-        }
+//        DB::beginTransaction();
+//        try {
+//           DB::update('
+//        UPDATE posts SET created_at = :create WHERE created_at IS NULL
+//        ', ['create' => NOW()]);
+//           DB::update('
+//        UPDATE posts SET updated_at = :update WHERE updated_at IS NULL
+//        ', ['update' => NOW()]);
+//           DB::commit();
+//        } catch (\Exception $e){
+//            DB::rollBack();
+//            echo $e->getMessage();
+//        }
+//
+//        $posts = DB::select('
+//            SELECT * FROM posts WHERE id > :id
+//            ',
+//            ['id' => 1]
+//        );
+//        return $posts;
+//
+//        return view('home', [
+//            'count' => 24,
+//            'name' => 'Yura'
+//        ]);
 
-        $posts = DB::select('
-            SELECT * FROM posts WHERE id > :id
-            ',
-            ['id' => 1]
-        );
-        return $posts;
-
-        return view('home', [
-            'count' => 24,
-            'name' => 'Yura'
-        ]);
+        $data =  DB::table('country')->get();
+        dd($data);
+        return $data;
     }
 }
