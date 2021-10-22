8967bbbf677c434642eba190d372a632b88ee83e
zhizhnevskiy <zhizhnevskiydev@gmail.com>
2021-10-22 14:32:04 +0300

    update

--- JS/JS/index.php
+++ JS/JS/index.php
@@ -7,13 +7,41 @@
 <body>
 
 <script>
-    alert('Hi from php file!');
-    // debugger;
+    // let user = {
+    //     name: "John",
+    //     age: 30,
+    //     isAdmin: true
+    // };
+    //
+    // for (let key in user) {
+    //     // ключи
+    //     console.log(key);  // name, age, isAdmin
+    //     // значения ключей
+    //     console.log(user[key] ); // John, 30, true
+    // }
+
+    let userTest = {};
+    userTest.name = 'John';
+    userTest.surname = 'Smith';
+    userTest.name = 'Pete';
+    console.log(userTest.name);
 </script>
 
-<script>console.log('Test JS!');</script>
 
-<script src="script.js"></script>
+<?php
+echo $today = date('Y-m-d') . '<br>';
+echo $todayMinus1day = date('Y-m-d', strtotime('-1 days')) . '<br>';
+echo $todayMinus7day = date('Y-m-d', strtotime('-7 days')) . '<br>';
+phpinfo();
+?>
+<!--<script>-->
+<!--    alert('Hi from php file!');-->
+<!--    // debugger;-->
+<!--</script>-->
+<!---->
+<!--<script>console.log('Test JS!');</script>-->
+<!---->
+<!--<script src="script.js"></script>-->
 
 </body>
 </html>
